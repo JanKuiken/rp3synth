e4eef6c315f477bac4c98e8cdec8ba4299d4aa31
Jan Kuiken (op laptop) <jan.kuiken@quicknet.nl>
2019-02-22 21:49:53 +0100

    Added more waveforms, including the wird waveform 'noise'

--- src/wave.cpp
+++ src/wave.cpp
@@ -5,15 +5,59 @@
 Wave::Wave(int in_rate)
 {
     rate = in_rate;
+
+    // strange stuff from https://en.cppreference.com/w/cpp/numeric/random/uniform_real_distribution
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    gen = std::mt19937(rd()); //Standard mersenne_twister_engine seeded with rd()
+    dis = std::uniform_real_distribution<>(-1.0, 1.0);
 }
 
-void Wave::Start(double in_frequency) {
+void Wave::Start(double in_frequency, const std::string& in_wave) {
     phi = 0.0;
     phi_step = M_PI * in_frequency / (double)rate;
+    waveform = StringToWaveform(in_wave);
+}
+
+WaveForm Wave::StringToWaveform(std::string in_wave) {
+    WaveForm wf = sine; // default, if no valid string is provided
+    if (in_wave == "sine")     wf = sine;
+    if (in_wave == "square")   wf = square;
+    if (in_wave == "triangle") wf = triangle;
+    if (in_wave == "sawtooth") wf = sawtooth;
+    if (in_wave == "noise")    wf = noise;
+    return wf;
 }
 
 double Wave::Next() {
-    double retval = sin(phi);
+    double retval;
+    double factor = phi / (2 * M_PI);
+    switch (waveform) {
+        case sine:
+            retval = sin(phi);
+        break;
+        case square:
+            retval = (factor < 0.5) ? 1.0 : -1.0;
+        break;
+        case triangle:
+            if (factor < 0.25) {
+                retval = factor * 4.0;
+            } else if (factor < 0.75) {
+                retval = 2.0 - factor * 4.0;
+            } else {
+                retval = -4.0 + factor * 4.0;
+            }
+        break;
+        case sawtooth:
+            if (factor < 0.5) {
+                retval = factor * 2.0;
+            } else {
+                retval = -2.0 + factor * 2.0;
+            }
+        break;
+        case noise:
+            retval = dis(gen);
+        break;
+    }
     phi += phi_step;
     if (phi >= 2 * M_PI) {
         phi -= 2 * M_PI;
--- src/wave.h
+++ src/wave.h
@@ -1,19 +1,36 @@
 #ifndef WAVE_H
 #define WAVE_H
 
+#include <string>
+#include <random>
+
+enum WaveForm { sine,
+                square,
+                triangle,
+                sawtooth,
+                noise
+              };
 
 class Wave
 {
 public:
     Wave(int in_rate);
 
-    void Start(double in_frequency);
+    void Start(double in_frequency, const std::string& in_wave);
     double Next();
 
+
 private:
-    int rate;          // samples per seconds
+
+    WaveForm StringToWaveform(std::string in_wave);
+
+    int rate;            // samples per seconds
+    WaveForm  waveform;
     double phi;
     double phi_step;
+
+    std::mt19937 gen;
+    std::uniform_real_distribution<> dis;
 };
 
 #endif // WAVE_H
