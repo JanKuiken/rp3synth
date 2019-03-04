ab6dacd7ef4cc2e8fa2a4e08131a7cc350541da5
Jan Kuiken (op laptop) <jan.kuiken@quicknet.nl>
2019-03-04 19:21:36 +0100

    made pitchbend more general

--- src/rp3synth.cpp
+++ src/rp3synth.cpp
@@ -56,9 +56,7 @@ void RP3Synth::MidiCallback(snd_seq_event_t *ev)
 
     switch (ev->type) {
         case SND_SEQ_EVENT_PITCHBEND:
-            // change value to -1.0... to 1.0 (at least for my keyboard)
-            voicegobals->pitch = ((ev->data.control.value-1) / 64) / 127.0;
-            std::cout << "pitchbend : " << voicegobals->pitch << std::endl;
+            voicegobals->pitch = bounds_limit((ev->data.control.value-1) / (64 * 127.0), -1.0, 1.0);
             break;
         case SND_SEQ_EVENT_CONTROLLER:
             std::cout << "modulator : " << ev->data.control.value << std::endl;
